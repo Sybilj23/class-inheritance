b17ea50366c57481a78fb24f486beced7754f713
tiffxyz <tiffanie.lyon@gmail.com>
2024-03-06 23:50:06 -0600

    all done

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,19 @@
 const Person = require('./person');
-
-// Your code here 
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience){
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+  static combinedYearsOfExperience(teachers) {
+    let sum = 0;
+    for (let i = 0; i < teachers.length; i++) {
+      sum += teachers[i].yearsOfExperience;
+    }
+    return sum;
+  }
+}
+// Your code here
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
